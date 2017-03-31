08a414d900bf4f43360ca88fae98105784525203
Adrian Hall <adrianha@microsoft.com>
2017-03-31 10:26:36 -0700

    Fixed Android Issues

--- TaskList/TaskList/TaskList.Android/MainActivity.cs
+++ TaskList/TaskList/TaskList.Android/MainActivity.cs
@@ -1,33 +1,28 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace TaskList.Droid
 {
-	[Activity (Label = "TaskList",
+    [Activity (Label = "TaskList.Droid",
         Icon = "@drawable/icon",
-        Theme="@style/MainTheme",
+        Theme="@android:style/Theme.Holo.Light",
         MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
 
             // Initialize Azure Mobile Apps
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
 
             // Initialize Xamarin Forms
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+			global::Xamarin.Forms.Forms.Init(this, bundle);
 
             // Load the main Application
-			LoadApplication (new TaskList.App ());
+			LoadApplication(new TaskList.App());
 		}
 	}
 }
